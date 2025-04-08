cdfc5bdde3d663595857984f337da37e5d3f6922
Cognet-74 <87704385+Cognet-74@users.noreply.github.com>
2025-04-08 21:23:36 +0200

    Bump version to 0.1.2 and rebuild for PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="minisecret",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["cryptography"],
@@ -16,7 +16,7 @@ setup(
     description="A minimal AES-256-GCM-based secrets manager for Python",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Cognet-74/MiniSecret",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
